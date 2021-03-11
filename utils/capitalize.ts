e7b75236a48fdc9c90523c4341c00a9d423e8bb5
Thomas <thomas.gazzoni@gmail.com>
2021-03-11 21:15:17 +0800

    fix: cookies

--- utils/capitalize.ts
+++ utils/capitalize.ts
@@ -1,5 +1,6 @@
 import title from 'title';
 
 export default function capitalize(text: string) {
+  text = text || '';
   return title(text.toString().replace(/-|_/g, ' '));
 }
--- utils/cookies.ts
+++ utils/cookies.ts
@@ -7,8 +7,10 @@ export function getCookieValue(key: string, defaultValue: any = undefined) {
 }
 
 export function setCookieValue(key: string, value: any) {
+  const expires = new Date();
+  expires.setDate(expires.getDate() + 180);
   return cookies.set(key, value, {
     path: '/',
-    expires: new Date(Date.now() + 2592000),
+    expires: expires,
   });
 }
